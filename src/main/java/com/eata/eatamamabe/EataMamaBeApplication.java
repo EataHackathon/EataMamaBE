0de09a8df6d31de0a57c4f34bf4c99755c4c4563
yangjunsig <wnstlr1379@naver.com>
2025-08-31 03:01:01 +0900

    first commit

---
+++ src/main/java/com/eata/eatamamabe/EataMamaBeApplication.java
@@ -0,0 +1,13 @@
+package com.eata.eatamamabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EataMamaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EataMamaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eata/eatamamabe/EataMamaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eata.eatamamabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EataMamaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
